11ccdb1a540c13e41563c80c2a28d278094b39b5
NajihaMamun <catsbrown60@gmail.com>
2026-07-12 08:43:33 +0600

    Add GitHub Pages base path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/class-11/', // 👈 Add this line
   plugins: [react()],
   server: {
     proxy: {
